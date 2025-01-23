450dc3f003bafead84df51b6170c38f82cf472d4
Leonardo Raupp <leonardoraupprodrigues@hotmail.com>
2025-01-22 23:37:17 -0300

    rename project

--- src/main/java/com/example/mongodb_springboot/SocialHubApplication.java
+++ src/main/java/com/example/mongodb_springboot/SocialHubApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MongodbSpringbootApplication {
+public class SocialHubApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MongodbSpringbootApplication.class, args);
+		SpringApplication.run(SocialHubApplication.class, args);
 	}
 
 }
